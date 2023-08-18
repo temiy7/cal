dd8b80731869190a7041cab2de3196452b8945b6
temiy7 <temiy7@gmail.com>
2023-08-18 12:46:15 +0900

    닥메계산 수정

--- script/다크메이지.js
+++ script/다크메이지.js
@@ -1,4 +1,3 @@
-
 function cal() {
 
     //변수
@@ -7,10 +6,10 @@ function cal() {
     var v2 = Number(document.getElementById("v2").value); // 최댐인챈트
     //마법최댐
     var v3 = Number(document.getElementById("v3").value); // 라볼트최댐
+    var v4 = $('input[name="v4"]').is(':checked') ? parseFloat($('input[name="v4"]').val()) : 1; //라체상채
 
     var v7 = Number(document.getElementById("v7").value); // 파이어볼최댐
     var v8 = Number(document.getElementById("v8").value); // 아습최댐
-
     //마스터리
     var v12 = Number(document.getElementById("v12").value); // 볼트마스터리
     var v14 = Number(document.getElementById("v14").value); // 라이트닝마스터리
@@ -19,35 +18,12 @@ function cal() {
 
 
     //아이템 타이틀
-    var v26v = document.getElementsByName("v26"); // 헤보나로브
-    var v26 = document.getElementById("v26")
-    for (var i = 0; i < v26v.length; i++) {
-        if (v26v[i].checked)
-            v26 = 0
-        else
-            v26 = 1;
-        break;
-    }
-    var v27v = document.getElementsByName("v27"); // 로즈마리장깁
-    var v27 = document.getElementById("v27")
-    for (var i = 0; i < v27v.length; i++) {
-        if (v27v[i].checked)
-            v27 = 0
-        else
-            v27 = 1;
-        break;
+    var v26 = $('input[name="v26"]').is(':checked') ? parseFloat($('input[name="v26"]').val()) : 0; // 헤보나로브
 
-    }
 
-    var v28v = document.getElementsByName("v28"); //블리안세트
-    var v28 = document.getElementById("v28")
-    for (var i = 0; i < v28v.length; i++) {
-        if (v28v[i].checked)
-            v28 = 0;
-        else
-            v28 = 1;
-        break;
-    }
+    var v27 = $('input[name="v27"]').is(':checked') ? parseFloat($('input[name="v27"]').val()) : 0; //로즈마리장
+
+    var v28 = $('input[name="v28"]').is(':checked') ? parseFloat($('input[name="v28"]').val()) : 0; //블리안세트
 
 
     var v29 = Number(document.getElementById("v29").value); //추댐
@@ -56,51 +32,39 @@ function cal() {
     var v30_2 = $("#v30 option:selected").attr('value2'); // 원드영향력
 
 
-
-
     var v31 = Number(document.getElementById("v31").value); //라이트닝원드영향력
 
     var v32 = Number(document.getElementById("v32").value); //추가크리대미지
 
-    var v33v = document.getElementsByName("v33"); //속성마스터 타이틀
-        var v33 = document.getElementById("v33") //
-    for (var i = 0; i < v33v.length; i++) {
-        if (v33v[i].checked)
-            v33 = 0;
-        else
-            v33 = 1;
-        break;
-    }
+    var v33 = $('input[name="v33"]').is(':checked') ? parseFloat($('input[name="v33"]').val()) : 0; //로즈마리장갑
 
 
     //계산식
 
     var res1; //런지
-    res1 = ((6000+v1*100)*(1+0.15+0.005*v15+0.15*v28)+((2760 + 21 * v1 + 0.5 * v2 + 15 * v7) * (1 + 0.15 + 0.005 * v15 +0.1*v33+0.15*v28))*v30_1)*v30 * (1 + v29 / 100);
+    res1 = ((6000 + v1 * 100) * (1 + 0.15 + 0.005 * v15 + 0.15 * v28) + ((2760 + 21 * v1 + 0.5 * v2 + 15 * v7) * (1 + 0.15 + 0.005 * v15 + 0.1 * v33 + 0.15 * v28)) * v30_1) * v30 * (1 + v29 / 100) * v4;
     document.getElementById("res1").value = Math.floor(res1);
 
     var res1_1; // 런지 크리
-    res1_1 = res1*(2.5+v32/100)
+    res1_1 = res1 * (2.5 + v32 / 100)
     document.getElementById("res1_1").value = Math.floor(res1_1);
 
     var res2; //체인
-    res2 = ((800+v1*11)*(1+0.15+0.005*v14+0.15*v28)+((180 + 0.6 * v1 + 0.44 * v2 + 2.5 * v3 + (7 + 0.75 * v31)*5*v30_2) *1.28 * (1 + 1.5 + 0.3 + 0.01 * v12 + 0.005 * v14+0.1*v33+0.15*v26+0.15*v28 ))) *v30* (1 + v29 / 100);
+    res2 = ((800 + v1 * 11) * (1 + 0.15 + 0.005 * v14 + 0.15 * v28) + ((180 + 0.6 * v1 + 0.44 * v2 + 2.5 * v3 + (7 + 0.75 * v31) * 5 * v30_2) * 1.28 * (1 + 1.5 + 0.3 + 0.01 * v12 + 0.005 * v14 + 0.1 * v33 + 0.15 * v26 + 0.15 * v28))) * v30 * (1 + v29 / 100);
     document.getElementById("res2").value = Math.floor(res2);
 
     var res2_1; //체인크리
-    res2_1 = res2*(2.5+v32/100)
+    res2_1 = res2 * (2.5 + v32 / 100)
     document.getElementById("res2_1").value = Math.floor(res2_1);
 
 
     var res3; //스톰
-    res3 = ((1000+v1*17)*(1+0.15+0.005*v16+0.15*v28) +((336 + 2.3 * v1 + 0.1 * v2 + 5 * v8) * (1 + 0.15 + 0.005 * v16 + 0.15 * v27 +0.1*v33+0.15*v28) *v30_1 ))*v30 * (1 + v29 / 100);
+    res3 = ((1000 + v1 * 17) * (1 + 0.15 + 0.005 * v16 + 0.15 * v28) + ((336 + 2.3 * v1 + 0.1 * v2 + 5 * v8) * (1 + 0.15 + 0.005 * v16 + 0.15 * v27 + 0.1 * v33 + 0.15 * v28) * v30_1)) * v30 * (1 + v29 / 100);
     document.getElementById("res3").value = Math.floor(res3);
 
     var res3_1//스톰크리
-    res3_1 = res3*(2.5+v32/100)
+    res3_1 = res3 * (2.5 + v32 / 100)
     document.getElementById("res3_1").value = Math.floor(res3_1);
-
-
 
 
 }
@@ -125,6 +89,7 @@ function reset() {
     document.getElementById("res2").value = "";
     document.getElementById("res3_1").value = "";
     document.getElementById("res3").value = "";
+    $("input:checkbox").prop("checked", false);
 
 
 }
