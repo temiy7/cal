5becddc2f5e136eda42345c5e66fa1681869b6ed
temiy7 <48972300+temiy7@users.noreply.github.com>
2023-11-25 03:32:54 +0900

    파볼 오류 수장

--- script/알스 대미지 계산.js
+++ script/알스 대미지 계산.js
@@ -7,15 +7,12 @@ function cal() {
 
     //기본댐
     var v1 = $("#v1 option:selected").attr('value'); // 매그넘 랭크별 대미지
-    var v1_1 = $("#v1 option:selected").attr('value2'); // 엘프 매그넘 랭크별 대미지
-    var v1_0 = $("#v1_0 option:selected").attr('value'); // 하이드로피어스 랭크별 대미지
-    var v1_01 = $("#v1_0 option:selected").attr('value2'); // 라스매시 인잭션 랭크별 대미지
     var v2 = $("#v2 option:selected").attr('value'); // 크래시샷 랭크별 대미지
-    var v2_1 = $("#v2 option:selected").attr('value2'); // 엘프크래시샷 랭크별 대미지
-    var v2_0 = $("#v2_0 option:selected").attr('value'); // 플레임버스트 랭크별 대미지
-    var v3 = $("#v3 option:selected").attr('value'); // 트라이어설 랭크별 대미지
-    var v3_0 = $("#v3_0 option:selected").attr('value'); // 파리택 랭크별 대미지
-    var v3_01 = $("#v3_0 option:selected").attr('value2'); // 파리택 랭크별 인잭
+    var v3 = $('input[name="v3"]').is(':checked') ? parseFloat($('input[name="v3"]').val()) : 0; //어전트샷
+    var v4 = $('input[name="v4"]').is(':checked') ? parseFloat($('input[name="v4"]').val()) : 0; //비전오브라데카
+
+
+
 
     var v5 = Number(document.getElementById("v5").value); // 전장비율
 
@@ -41,7 +38,21 @@ function cal() {
 
     //세공
     var v13 = Number(document.getElementById("v13").value); //매그넘샷 대미지
-    var v19 = Number(document.getElementById("v19").value); //파편 대미지
+    var v14 = Number(document.getElementById("v19").value); //파편 대미지
+    var v15 = Number(document.getElementById("v20").value); //불연금효율
+    var v16 = Number(document.getElementById("v21").value); //물연금효율
+    // var v17 = Number(document.getElementById("v22").value); //바람연금 효율
+    // var v18 = Number(document.getElementById("v23").value); //흑연금 효율
+
+
+    // 추가
+
+
+    var v21 = $('input[name="v21"]').is(':checked') ? parseFloat($('input[name="v21"]').val()) : 0; //증발
+    var v22 = $('input[name="v22"]').is(':checked') ? parseFloat($('input[name="v22"]').val()) : 0; //링크보너스
+    var v23 = $('input[name="v23"]').is(':checked') ? parseFloat($('input[name="v23"]').val()) : 0; //최대거리
+
+
 
 
     //계산식
@@ -53,15 +64,37 @@ function cal() {
     var v6; //추가크리
     v6 = v6_0 + v6_1 + v6_2 + 7 * v6_3;
     document.getElementById("v6").value = v6;
-
+    
     var res0; //레인지샷 대미지
-    res0 = (v0 * (1 + 0.2 * v8) * (1 + (v5 / 100)) * (1 + 0.2 * v8)) * (1 + v7 / 100) * (1 + v12) * (1 + v3 * v20) * (1 * v00 + 2 * v00_1 * (1 + 1 * v21 * v11))
+    res0 = (v0 * (1 + 0.2 * v8) * (1 + (v5 / 100)) * (1 + 0.2 * v8)) * (1 + v7 / 100) * (1 + v12) 
     document.getElementById("res0").value = Math.floor(res0);
 
+    
+    var res0_1; //알케믹 시너지
+    res0_1 = (1+0.15+0.01*v20+0.01*v21-0.25)
+    if(res0_1<1){res0_1=1}
+    else{res0_1=res0_1}
+    document.getElementById("res0_1").value = Math.floor(res0_1);
 
     var res1; //매그넘샷 대미지
     res1 = res0 * (1 * v00 + 0.5 * v00_1) * (1 * v1 + 0.1 * v13) * (1 + 0.15 * v9 + v7 / 100)
     document.getElementById("res1").value = Math.floor(res1);
+
+    var res2; //크래시샷 파편댐
+    res2 = res0 * (1 * v00 + 0.5 * v00_1) * (1 * v2 + 0.035 * v14 + v9 / 100) * (1 + 1 * v11)*(1 + v7 / 100)
+    document.getElementById("res2").value = Math.floor(res2);
+
+    var res3; //하이드로피어스 대미지
+    res3 = res1+ res0 *18 *(1 + v7 / 100)*(1+0.15*v22)  
+    document.getElementById("res3").value = Math.floor(res3);
+
+    var res4; //플레임버스트 대미지    
+    res4 = (res0 * 4 *(1 + v7 / 100)  + res2)*res0_1;
+    document.getElementById("res4").value = Math.floor(res4);
+
+    var res5; //
+
+
 
 
 }
